327f09a3be593780ea5e89086d1439866f8452f7
Paul Guzmán <andres22.pgpa@gmail.com>
2020-08-14 15:03:34 -0400

    update-2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      click: null
+    }
+  }  
+
+  render() {
+    console.log(this.state);
+
+    let redEndncido = '';
+    if(this.state.click === 'red') redEndncido = 'encendido';
+    let yellowEndncido = '';
+    if(this.state.click === 'yellow') yellowEndncido = 'encendido';
+    let greenEndncido = '';
+    if(this.state.click === 'green') greenEndncido = 'encendido';
+
+    return (
+
+      <>
+
+        <div id="barra"></div>
+        <div id="semaforo">
+          <div className={"luz roja "+redEndncido} onClick={() => this.setState({click: 'red'})}>
+          </div>
+          <div className={"luz amarilla "+yellowEndncido} onClick={() => this.setState({click: 'yellow'})}>
+          </div>
+          <div className={"luz verde "+greenEndncido} onClick={() => this.setState({click: 'green'})}>
+          </div>
+        </div>
+
+      </>
+    )
+  }
+}
+  export default App;
